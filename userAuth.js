cb42eade90a3f890b010a08e7ed967e1ccd12ced
RidwanAdebosin <adebosinadewale@gmail.com>
2024-05-15 15:48:20 +0100

    new userAuth

--- userAuth.js
+++ userAuth.js
@@ -1,35 +1,45 @@
-const database = [
-  { email: "dan@gmail.com", password: "dan223", name: "dan" },
-  { email: "oba@gmail.com", password: "oba223", name: "oba" },
-  { email: "ekun@gmail.com", password: "ekun223", name: "ekun" },
-  { email: "tiger@gmail.com", password: "tiger223", name: "tiger" },
-  { email: "lion@gmail.com", password: "lion223", name: "lion" },
-];
+/*create a function that takes two array of objects e.g 
+[
+  {name: "Ridwan", stack: "Frontend"}, 
+  {name: "Rapheal", stack: "Backend"},
+  {name: "Kelechi", stack: "Frontend"},
+  {name: "Tega", stack: "Backend"},
+] 
+and 
+[
+  {name: "Ridwan", assignment1: 18, assignment2: 17, assignment3: 20}, 
+  {name: "Rapheal", assignment1: 17, assignment2: 17},
+  {name: "Kelechi", assignment1: 19, assignment2: 17, assignment3: 20},
+  {name: "Tega", assignment1: 19, assignment2: 17},
+] 
+and returns a new array that has the name and average Assignment Score.
+*/
 
-const userAuthentication = (userEmail, userPassword) => {
-  // Find the user in the database
-  const userData = database.find(
-    (user) => user.email === userEmail && user.password === userPassword
-  );
+const db1 = [
+  {name: "Ridwan", stack: "Frontend"}, 
+  {name: "Rapheal", stack: "Backend"},
+  {name: "Kelechi", stack: "Frontend"},
+  {name: "Tega", stack: "Backend"},
+]
 
-  // Check if user exists
-  if (userData) {
-    console.log("You are welcome");
-  } else {
-    // If the provided log-in credentials are incorrect, display this error message
-    console.log("User not found!");
-    return;
-  }
+const db2 = [
+  {name: "Ridwan", assignment1: 18, assignment2: 17, assignment3: 20}, 
+  {name: "Rapheal", assignment1: 17, assignment2: 17},
+  {name: "Kelechi", assignment1: 19, assignment2: 17, assignment3: 20},
+  {name: "Tega", assignment1: 19, assignment2: 17},
+]
 
-  // Check if the email or password is incorrect
-  if (userData.email !== userEmail) {
-    console.log("Incorrect email. Please try again.");
-  }
-  if (userData.password !== userPassword) {
-    console.log("Incorrect password. Please try again.");
-  }
-};
+const getAverage = (array1, array2) => {
+  const average = array2.map((el, ind) => {
+    if (array1[array2.findIndex((e) => e.name === array2[ind].name)].stack ==="Frontend"){
+      return {name: el.name, averageAssignment: (el.assignment1 + assignment2 + assignment3) / 3}
+    } else{
+      return {name: el.name, averageAssignment: (el.assignment1 + assignment2) / 2}
+    }
+  })
 
-// userAuthentication("lion@gmail.com", "lion223"); // Should print "You are welcome"
-// console.log(userAuthentication("lion@gmail.com", "wrongpassword")); // Should print "Incorrect password. Please try again."
-console.log(userAuthentication("wrongemail@gmail.com", "lion223")); // Should print "Incorrect email. Please try again."
+
+  return average;
+} 
+
+console.log(getAverage(db1, db2))
